fa79d45ebaf07bbc049d1dbe6075934694b9d75f
Kamil Purgat <kpurgat.zsp9@gmail.com>
2026-01-14 19:33:36 +0100

    "this." keyword

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -3,6 +3,8 @@ const app = Vue.createApp({
         return {
             courseGoal: 'Finish the course and learn Vue!',
             vueLink: 'https://vuejs.org/',
+            firstVariable: 'First variable',
+            secondVariable: 'Second variable',
         }
     },
     methods: {
@@ -14,6 +16,15 @@ const app = Vue.createApp({
             else {
                 return 'Deep dive into Vue!'
             }
+        },
+        variableFunction() {
+            const randomNumber = Math.random();
+            if(randomNumber < 0.5) {
+                return this.firstVariable;
+            } 
+            else {
+                return this.secondVariable;
+            }            
         }
     }
 })
